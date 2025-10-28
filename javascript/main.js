de44bfa0c0370882902a3f9f8eb91b315bb7cc54
Mouad Gourita <mouad.gourita@gmail.com>
2025-10-28 22:02:09 +0100

    finished adding a function to add a new card next is adding the Descriptiong and the amount to the Card

--- javascript/main.js
+++ javascript/main.js
@@ -11,12 +11,6 @@ document.getElementById("Balance").innerHTML = Balance + 'DH' ;
 
 
 
-document.getElementById("btn").addEventListener("click", function(){
-
-      document.querySelector(".popup").style.display = "block";
-
-});
-
 /* dyal dark mode */
 document.getElementById('themeToggle').addEventListener('click', function() {
      document.body.style.backgroundColor = "#000000" ;
@@ -29,35 +23,59 @@ document.getElementById('themeToggle').addEventListener('click', function() {
 document.getElementById("btn-positive").addEventListener('click', function() {
     let input1 = document.getElementById("input1").value;
     let input2 = document.getElementById("input2").value;
-     
+    
+    if (input1.trim() === '' || input2.trim() === '') {
+        alert("You Can't Submit it Empty");
+        return;
+    }
+
+    console.log(input1)
     let number = parseInt(input2);
 
     Income = Income + number ;
     document.getElementById("Income").innerHTML = '+' + Income + 'DH' ;
     Balance = Income - Spent ;
     document.getElementById("Balance").innerHTML = Balance + 'DH' ;
-
+    
     const myDiv = document.getElementById('Card');
-
     const newDiv = document.createElement('div');
-    newDiv.className = 'card-2 rounded-xl bg-red-200';
+    newDiv.className = 'h-40 shadow rounded-xl bg-green-200 hover:scale-105 transition-all';
     newDiv.innerHTML = `
-    <section class="popup">
-      <ul>
-        <li>
-          <a href="#">Edit</a>
-          <a href="#">Remove</a>
-          <a href="#">Edit</a>
-        </li>
-      </ul>
-    </section>
-    <button class="btn2" type="submit">...</button>`;
+    <div class="flex justify-around gap-2">
+      <div class="Card-menu hidden justify-center items-center bg-green-500 rounded-xl h-5 w-30">
+        <ul>
+          <li class="flex gap-4 text-xs m-2">
+            <a class="hover:text-red-700 hover:scale-105 transition-all" href="#">Edit</a>
+            <a class="hover:text-red-700 hover:scale-105 transition-all" href="#">Remove</a>
+          </li>
+        </ul>
+      </div>
+      <button id="btn" class=" text-lg hover:text-red-700 hover:scale-150 transition-all" type="submit">
+        <img class="w-6" src="./img/menu.png" alt="3 dots for menu">
+      </button>
+    </div>`;
+
+    myDiv.appendChild(newDiv);
+
+    document.getElementById("input1").value = '';
+    document.getElementById("input2").value = '';
+
+    document.getElementById("btn").addEventListener("click", function(){
+
+         document.querySelector(".Card-menu").style.display = "flex";
+  });
+
 });
 
 document.getElementById("btn-negative").addEventListener('click', function() {
      let input1 = document.getElementById("input1").value;
      let input2 = document.getElementById("input2").value;
      
+     if (input1.trim() === '' || input2.trim() === '') {
+        alert("You Can't Submit it Empty");
+        return;
+    }
+
      let number = parseInt(input2);
 
      Spent = Spent + number ;
