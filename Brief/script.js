bf0b15894a9d3f663a4953421e21031a3912df55
Mouad Gourita <mouad.gourita@gmail.com>
2025-10-28 12:17:25 +0100

    added js variables to toggle the changes in income and Balance/ Expenses

--- Brief/script.js
+++ Brief/script.js
@@ -1,8 +1,14 @@
 document.getElementById('themeToggle').addEventListener('click', function() {
      document.body.style.backgroundColor = "black" ;
-     
+     document.body.style.display= 'none';
 });
 
 
-let Spent = 2000;
-Spent = document.getElementById("Spent").innerHTML;
+let Spent = 10;
+document.getElementById("Spent").innerHTML = Spent + '$' ;
+
+let Income = 15;
+document.getElementById("Income").innerHTML = Income + '$' ;
+
+let Balance = 10;
+document.getElementById("Balance").innerHTML = Balance + '$' ;
